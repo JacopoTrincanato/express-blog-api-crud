01fc5292d2b309a74cd6a850f658a8383b93c6ac
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-05 15:14:18 +0100

    create database folder, add post array and export it

---
+++ database/db.js
@@ -0,0 +1,41 @@
+//creo l'array di post
+const posts = [
+    {
+      id: 1,
+      titolo: "Introduzione a JavaScript",
+      contenuto: "JavaScript è un linguaggio di programmazione molto utilizzato per lo sviluppo web.",
+      autore: "Mario Rossi",
+      data: "2024-10-25"
+    },
+    {
+      id: 2,
+      titolo: "Novità in ES2023",
+      contenuto: "Scopri le nuove funzionalità introdotte in ECMAScript 2023.",
+      autore: "Luigi Bianchi",
+      data: "2024-10-30"
+    },
+    {
+      id: 3,
+      titolo: "Guida ai CSS Flexbox",
+      contenuto: "Flexbox è un potente sistema per creare layout flessibili con CSS.",
+      autore: "Carla Verdi",
+      data: "2024-11-02"
+    },
+    {
+      id: 4,
+      titolo: "Come funziona l'asincronicità in JavaScript",
+      contenuto: "In questo post esploriamo le promesse e async/await in JavaScript.",
+      autore: "Anna Gialli",
+      data: "2024-11-03"
+    },
+    {
+      id: 5,
+      titolo: "I migliori editor per sviluppatori",
+      contenuto: "Analisi dei migliori editor per sviluppatori JavaScript nel 2024.",
+      autore: "Marco Neri",
+      data: "2024-11-05"
+    }
+];
+
+//esporto i post
+module.exports = posts
