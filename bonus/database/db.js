152b00ec2cbbe6954e37f3366186acd84bb72f9d
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-06 17:16:52 +0100

    put double quotes in automobili array keys

--- bonus/database/db.js
+++ bonus/database/db.js
@@ -1,53 +1,53 @@
 const automobili = [
     { 
-        marca: "Fiat",
-        modello: "Panda",
-        alimentazione: "Benzina"
+        "marca": "Fiat",
+        "modello": "Panda",
+        "alimentazione": "Benzina"
     },
     { 
-        marca: "Volkswagen",
-        modello: "Golf",
-        alimentazione: "Diesel"
+        "marca": "Volkswagen",
+        "modello": "Golf",
+        "alimentazione": "Diesel"
     },
     { 
-        marca: "Tesla",
-        modello: "Model 3",
-        alimentazione: "Elettrico"
+        "marca": "Tesla",
+        "modello": "Model 3",
+        "alimentazione": "Elettrico"
     },
     { 
-        marca: "Ford",
-        modello: "Focus",
-        alimentazione: "GPL"
+        "marca": "Ford",
+        "modello": "Focus",
+        "alimentazione": "GPL"
     },
     { 
-        marca: "Toyota",
-        modello: "Yaris",
-        alimentazione: "Ibrido"
+        "marca": "Toyota",
+        "modello": "Yaris",
+        "alimentazione": "Ibrido"
     },
     { 
-        marca: "Audi",
-        modello: "A4",
-        alimentazione: "Diesel"
+        "marca": "Audi",
+        "modello": "A4",
+        "alimentazione": "Diesel"
     },
     { 
-        marca: "BMW",
-        modello: "Serie 3",
-        alimentazione: "Benzina"
+        "marca": "BMW",
+        "modello": "Serie 3",
+        "alimentazione": "Benzina"
     },
     { 
-        marca: "Renault",
-        modello: "Clio",
-        alimentazione: "GPL"
+        "marca": "Renault",
+        "modello": "Clio",
+        "alimentazione": "GPL"
     },
     { 
-        marca: "Peugeot",
-        modello: "208",
-        alimentazione: "Elettrico"
+        "marca": "Peugeot",
+        "modello": "208",
+        "alimentazione": "Elettrico"
     },
     { 
-        marca: "Hyundai",
-        modello: "i30",
-        alimentazione: "Metano"
+        "marca": "Hyundai",
+        "modello": "i30",
+        "alimentazione": "Metano"
     }
 ];
 
