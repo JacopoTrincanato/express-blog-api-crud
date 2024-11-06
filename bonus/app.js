9726337e130b9aa1f931022f0a3c9a9f6d47a30d
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-06 16:09:17 +0100

    create controllers folder in bonus folder and move index and show in autocontroller.js

--- bonus/app.js
+++ bonus/app.js
@@ -6,30 +6,10 @@ const app = express();
 app.listen(3006, ()=>{
     console.log('Server is running on port 3006');
     
-})
-
-const automobili = require('../bonus/database/db.js');
+});
 
 //creo index
-app.get('/', (req, res)=>{
-    res.json({
-        data: automobili,
-        count: automobili.length
-    });
-});
+app.get('/', );
 
 //creo show
-app.get('/:marca', (req, res)=>{
-
-    //trovo l'automobile
-    const automobile = automobili.find(automobile=> automobile.marca === req.params.marca);
-
-    //restituisco un messaggio di errore se l'automobile non è presente
-    if (!automobile) {
-        return res.status(404).json({error: `Non sono presenti automobili della marca ${req.params.marca}`})
-    }
-
-    return res.status(200).json({
-        data: automobile
-    });
-});
+app.get('/:marca', );
