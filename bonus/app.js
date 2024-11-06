f12ad31d1b2e7b0d68fec08883bf00803265928e
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-06 16:04:09 +0100

    create show in app.js

--- bonus/app.js
+++ bonus/app.js
@@ -15,5 +15,21 @@ app.get('/', (req, res)=>{
     res.json({
         data: automobili,
         count: automobili.length
-    })
-})
+    });
+});
+
+//creo show
+app.get('/:marca', (req, res)=>{
+
+    //trovo l'automobile
+    const automobile = automobili.find(automobile=> automobile.marca === req.params.marca);
+
+    //restituisco un messaggio di errore se l'automobile non è presente
+    if (!automobile) {
+        return res.status(404).json({error: `Non sono presenti automobili della marca ${req.params.marca}`})
+    }
+
+    return res.status(200).json({
+        data: automobile
+    });
+});
