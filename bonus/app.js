516344ccce2b70be5668fa5c85bb93e3c7f08c3c
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-06 15:47:00 +0100

    create server on port 3006 in app.js bonus

---
+++ bonus/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+//creo il server e lo faccio ascoltare sulla porta 3006
+app.listen(3006, ()=>{
+    console.log('Server is running on port 3006');
+    
+})
