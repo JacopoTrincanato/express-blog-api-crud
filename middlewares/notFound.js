a5a3903b4f2bdfcff899bc5436bee0ddf0305c6f
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-07 12:58:51 +0100

    add error 500 to notFound.js

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -1,6 +1,16 @@
 //creo la costante notFound
 const notFound = (req, res, next)=>{
-    res.status(404).send('Non è possibile trovare la risorsa')
-}
+    res.status(404).send('Non è possibile trovare la risorsa');
+};
 
-module.exports = notFound;
+//gestisco l'errore 500
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send({
+        status: 500,
+        message: 'error message'
+
+    });
+});
+
+module.exports = notFound
